862607158b9049421f4770d90d393294c29846a1
diegonacif <diegoreisnacif@gmail.com>
2024-06-10 22:09:15 -0300

    fix: firebase config error on deploy

--- src/services/firebase.config.ts
+++ src/services/firebase.config.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
@@ -15,4 +15,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
